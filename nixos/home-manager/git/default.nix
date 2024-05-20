920542a11eed99af40b3982c71be3a3fd3c1ece2
Ethorbit <Ethorbit@protonmail.com>
2024-05-20 16:44:40 -0700

    Move .gitconfig to nix

--- nixos/home-manager/git/default.nix
+++ nixos/home-manager/git/default.nix
@@ -1,13 +1,42 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
-    home-manager.users.${config.ethorbit.users.primary.username} = {
-        programs.git = {
-            enable = true;
-        };
+    options.ethorbit.home-manager.git = with lib; {
+        user = {
+            email = mkOption {
+                description = "Your GitHub email";
+                default = "Ethorbit@protonmail.com";
+            };
 
-        home.file.".gitconfig" = {
-            source = ./config/.gitconfig;
+            name = mkOption {
+                description = "Your GitHub username";
+                default = "Ethorbit";
+            };
+        };
+    };
+
+    config = {
+        home-manager.users.${config.ethorbit.users.primary.username} = {
+            programs.git = {
+                enable = true;
+            };
+
+            home.file.".gitconfig" = {
+                text = ''
+                [user]
+                    email = ${config.ethorbit.home-manager.git.user.email}
+                    name = ${config.ethorbit.home-manager.git.user.name}
+                [credential]
+                    helper = cache
+                [pull]
+                    ff = true
+                [filter "lfs"]
+                    process = git-lfs filter-process
+                    required = true
+                    clean = git-lfs clean -- %f
+                    smudge = git-lfs smudge -- %f
+                '';
+            };
         };
     };
 }
